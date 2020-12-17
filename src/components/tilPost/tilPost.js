fdc6ef87adb2c4fce7dc31738c81f502af8d0a6c
Jenny Allton-Twist <jennyalltontwist@gmail.com>
2020-12-17 10:52:29 +0000

    updated bio css so bio doesn't overrun box at narow widths. added meta tags to tilpost component class

--- src/components/tilPost/tilPost.js
+++ src/components/tilPost/tilPost.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react';
 import './tilPost.css';
+import MetaTags from 'react-meta-tags';
+
 
 export class TilPost extends Component {
 
     render() {
         return (
-            <div key={this.props.i} id={this.props._id} className="til_form">
-                <div className="flex_til_titles"><p>Posted by: {this.props.username} </p>
-                    <p>Share</p>
-                    <p>Posted at: {this.props.formatDate(this.props.createdAt)}</p>
+            <>
+                <div className="wrapper">
+                    <MetaTags>
+                        <title>TILTime</title>
+                        <meta name={this.props.username} content={this.props.tilPost} />
+                        <meta property="og:url" content=""/>
+                        <meta property="og:title" content="TILTime Learning App" />
+                        <meta property="og:image" content="%PUBLIC_URL%/TILTime-favicon.png" />
+                        <meta name="twitter:title" content="TILTime Learning App" />
+                        <meta name="twitter:image" content="%PUBLIC_URL%/TILTime-favicon.png" />
+                    </MetaTags>
                 </div>
-                <p className="til_post_content">{this.props.tilPost}</p>
-            </div>
+                <div key={this.props.i} id={this.props._id} className="til_form">
+                    <div className="flex_til_titles"><p>Posted by: {this.props.username} </p>
+                        <p>Share</p>
+                        <p>Posted at: {this.props.formatDate(this.props.createdAt)}</p>
+                    </div>
+                    <p className="til_post_content">{this.props.tilPost}</p>
+                </div>
+                </>
         )
     }
 }
