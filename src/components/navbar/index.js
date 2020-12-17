1089179e805266b400bdbed3a99215eee2d25f52
Josh Field <josh.t.field@bigpond.com>
2020-12-17 15:24:07 +0000

    Added logout function

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,17 +4,19 @@ import './index.css';
 import { CgProfile } from 'react-icons/cg';
 import { CgFeed } from 'react-icons/cg';
 import { CgLogOut } from 'react-icons/cg';
+import axios from "axios";
 
 
 export class NavBar extends Component {
+
     render() {
         return (
             <nav className="navigation">
                 <Link link={`/profile/${this.props.currentUser}`} name=<CgProfile /> />
                 <Link link="/timeline" name=<CgFeed /> />
-                <div className="logout">
-                    <Link link="/logout" name=<CgLogOut /> />
-                </div>
+                <button onClick={this.props.logout} className="logout">
+                    <CgLogOut />
+                </button>
             </nav>
         )
     }
